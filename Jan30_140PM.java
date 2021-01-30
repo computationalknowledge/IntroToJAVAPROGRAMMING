472c3dedbd34d2309b5447559bf4ad4af7ca6651
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-01-30 13:37:47 -0500

    Create Jan30_140PM.java

---
+++ Jan30_140PM.java
@@ -0,0 +1,10 @@
+public	class Peanut {
+ 		// use comments to keep track of what you are doing
+
+ 		public static void main(String[] args){
+ 			// we can compile without errors 
+ 			// But it is not doing anything!
+ 			// let's add some workflow
+ 			// let's start by committing our starter code to github
+ 		}
+ 	}
