1be3336e17a1f3b772d2158d256b7a19689145e1
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-01-30 10:52:21 -0500

    Create GameCodeStarter1.java

---
+++ GameCodeStarter1.java
@@ -0,0 +1,11 @@
+public class Peanut {
+	// implementation in the curly brackets
+	// add comments with this double fore splash
+
+	public static void main(String[] args){
+		// implementation in the curly brackets
+		String name = args[0]; 
+		System.out.println("Let's play a game ", name);
+
+	}
+}
