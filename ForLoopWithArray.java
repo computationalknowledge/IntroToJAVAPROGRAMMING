63229b9725c8f0c9e863cd098be4225f0dab282e
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-01-30 14:01:19 -0500

    Create ForLoopWithArray.java

---
+++ ForLoopWithArray.java
@@ -0,0 +1,23 @@
+ public	class Peanut {
+ 		// use comments to keep track of what you are doing
+
+ 		public static void main(String[] inputData){
+ 			// args are the arguments, parameters
+ 			// we pass into our program
+ 			// when we start running it
+ 			// [] means array
+ 			// how do I input parameters passed in at the command line
+ 			
+ 			// loops: for loops, while loop, do while, foreach
+ 			// for loop
+ 			// array is an OBJECT - objects have data properties
+ 			// array know how many elements they have!
+ 			// 3 execution contexts: EC is a place where you can write some code
+ 			
+ 			for (int i = 0 ; i<inputData.length ; i++) {
+ 				System.out.println("Hello " + inputData[i] );
+ 			}
+ 			
+
+ 		}
+ 	}
